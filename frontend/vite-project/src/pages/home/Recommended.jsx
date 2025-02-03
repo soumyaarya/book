5bb67508364159f289ecad462d1aff118588d4f5
Soumya Arya <154082784+soumyaarya@users.noreply.github.com>
2025-02-03 11:56:34 +0530

    Update Recommended.jsx

--- frontend/vite-project/src/pages/home/Recommended.jsx
+++ frontend/vite-project/src/pages/home/Recommended.jsx
@@ -7,7 +7,7 @@ import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import BookCard from '../books/bookCard';
+import BookCard from '../books/BookCard';
 import { useFetchAllBooksQuery } from '../../redux/features/books/books.Api';
 
 const Recommended = () => {
