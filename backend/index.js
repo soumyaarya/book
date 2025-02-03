e1e2294bb7fca295e33c7f87948d7d930e902e5a
Soumya Arya <154082784+soumyaarya@users.noreply.github.com>
2025-02-03 15:02:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const mongoose = require('mongoose')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+app.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+  });
 //middleware
 app.use(express.json())
 app.use(cors({
