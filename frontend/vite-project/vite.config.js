0c4af8ce05fe1f0a30d3a8a7175fb9a60f1b6d50
Soumya Arya <154082784+soumyaarya@users.noreply.github.com>
2025-02-03 15:45:57 +0530

    Update vite.config.js

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // Proxy requests to the backend
+      '/api': 'https://book-backend-git-main-soumya-aryas-projects.vercel.app/', // Proxy requests to the backend
     },
   },
   
