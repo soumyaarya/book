d6806c3be63eb4ff96f91fd5ed6c72bd434f98b3
Soumya Arya <154082784+soumyaarya@users.noreply.github.com>
2025-02-03 11:52:49 +0530

    Update router.jsx

--- frontend/vite-project/src/routers/router.jsx
+++ frontend/vite-project/src/routers/router.jsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from "react-router-dom"
 import App from "../App";
-import Home from "../pages/home/home";
+import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
